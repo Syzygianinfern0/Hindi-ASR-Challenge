8793618aa8f0b7bd0bdbb8db11efc77a0a5c0f71
Syzygianinfern0 <spsharan2000@gmail.com>
2020-06-19 19:10:20 +0530

    Install script for Kaldi

---
+++ tools/quickstart.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Modified version of https://github.com/espnet/espnet/blob/master/ci/install_kaldi.sh
+
+set -euo pipefail
+
+echo "Cloning Kaldi"
+[ ! -d kaldi ] && git clone https://github.com/kaldi-asr/kaldi --depth 1
+(
+  cd kaldi/tools || exit 1
+  echo "" >extras/check_dependencies.sh
+  chmod +x extras/check_dependencies.sh
+  echo "Compiling the necessary packages..."
+  make sph2pipe sclite >/dev/null
+)
+
+echo "Downloading the pre-build kaldi binary"
+[ ! -e ubuntu16-featbin.tar.gz ] && wget --tries=3 https://github.com/espnet/kaldi-bin/releases/download/v0.0.1/ubuntu16-featbin.tar.gz
+tar -xf ./ubuntu16-featbin.tar.gz
+cp featbin/* kaldi/src/featbin/
